f1bd294fbf2c038c4dcfa0fec0ccfc4c1bf84fd6
DESKTOP- caleb <caleb.wigwubgames@gmail.com>
2026-04-18 13:22:34 -0400

    test: set SERVER_URL to ngrok tunnel for cross-machine test

--- client/autoload/config.gd
+++ client/autoload/config.gd
@@ -3,4 +3,4 @@ extends Node
 # Change this one line to switch between local dev and ngrok.
 # Local:  ws://localhost:8765
 # ngrok:  wss://xxxx-xx-xx-xx-xx.ngrok-free.app
-const SERVER_URL: String = "ws://localhost:8765"
+const SERVER_URL: String = "wss://untying-cornstalk-irritable.ngrok-free.dev"
